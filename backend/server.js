bc3df7e55ea2b209e71a0a48a9da0ba7344f0dc0
Lalithya Golla <108351773+lalithyagolla@users.noreply.github.com>
2025-06-14 15:00:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express()
 app.use(cors())
-app.use*express.json()
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log("MONGO DB COnnected successfully"))
