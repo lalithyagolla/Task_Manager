eb29981b9e61903459b05198ceef157101acb40b
Lalithya Golla <108351773+lalithyagolla@users.noreply.github.com>
2025-06-14 14:55:06 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API;
+
+function App(){
+  const [title,setTitle] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const fetchTasks = async () => {
+    const res = await axios.get(`${API}/api/tasks`);
+    setTasks(res.data)
+  }
+
+  const handleAdd = async () => {
+    if(!title) return;
+    await axios.post(`${API}/api/tasks`,{title})
+    setTitle('')
+    fetchTasks()
+  }
+
+  const handleDelete = async (id) => {
+    await axios.delete(`${API}/api/tasks/${id}`)
+    fetchTasks()
+  }
+
+  useEffect(() => {
+    fetchTasks
+  },[])
+
+  return(
+    <div style={{padding:"2em"}}>
+      <h2>TASK MANAGER</h2>
+      <input value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      placeholder='New Task'/>
+      <button onClick={handleAdd}>ADD</button>
+      <ul>
+        {tasks.map(task => {
+          <li key = {task._id}>
+            {task.title} &nbsp;
+            <button onClick={()=> handleDelete}>Delete</button>
+          </li>
+        })}
+      </ul>
+
+    </div>
+  )
+}
+export default App;
